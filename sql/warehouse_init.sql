5d896f41834bf52649d82cbc02a514f3321c0c14
Papy T <adediwuraolamilekan@gmail.com>
2026-05-31 19:44:04 -0400

    First commit: project basics - docker-compose, Dockerfile, sql init scripts

--- sql/warehouse_init.sql
+++ sql/warehouse_init.sql
@@ -2,5 +2,5 @@
 -- Creates the schemas dlt and dbt will write into
 
 CREATE SCHEMA IF NOT EXISTS raw;      -- dlt writes here (copy of lake data)
-CREATE SCHEMA IF NOT EXISTS staging;  -- dbt staging models live here
+CREATE SCHEMA IF NOT EXISTS staging;  -- dbt staging models live here 
 CREATE SCHEMA IF NOT EXISTS marts;    -- dbt dimension + fact tables live here
